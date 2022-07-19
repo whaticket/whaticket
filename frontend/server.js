fffaa466d5e7aa854d25cea12cbcea8f7623bd48
Alan Martines <alancpmartines@hotmail.com>
2022-07-19 17:24:52 -0400

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -6,4 +6,6 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port: ${process.env.PORT}`);
+});
